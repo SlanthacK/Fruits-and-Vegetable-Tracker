9f2d114badc2f2025296101a99cc912bcf50999b
vazravasu <suryasai.venkatesh@gmail.com>
2019-02-12 23:37:12 +0530

    created transction apprv_req_pDistributor_to_farmer

--- foodveg.js
+++ foodveg.js
@@ -33,7 +33,7 @@ async function req_p_to_f(req)
     }
     else if (req.fandv.req_state=='Requested_by_pDistributor')
     {
-        throw new window.alert("it has been already requested");
+        throw new window.alert("it already has been requested");
     }
     else if (req.fandv.req_state=='approved_by_farmer')
     {
@@ -51,3 +51,24 @@ async function req_p_to_f(req)
     .then(function updateRegistry(assetRegistry)
           {return assetRegistry.update(req.fandv)});
 }
+
+/**
+ * farmer will approve request from primarey distributor
+ * @param {org.xyz.foodvegtracker.apprv_req_pDistributor_to_farmer} apprv
+ * @transaction
+ */
+
+ async function apprv_req_farmer(apprv)
+ {
+     if(apprv.fandv.req_state=='Requested_by_pDistributor' && apprv.fandv.owner=='FARMER')
+     {
+         apprv.fandv.req_state='approved_by_farmer';
+     }
+     else if (apprv.fandv.req_state=='approved_by_farmer')
+     {
+         throw new window.alert("it already has been approved")
+     }
+     return getAssetRegistry('org.xyz.foodvegtracker.FandV')
+     .then(function updateRegistry(assetRegistry)
+           {return assetRegistry.update(apprv.fandv)});
+ }
